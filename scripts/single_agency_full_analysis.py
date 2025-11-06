07747d8e071d4a7deeada4c19a5c1c3287f560ab
YiZoeHe <113316748+YiZoeHe@users.noreply.github.com>
2025-11-06 09:45:26 -0700

    Add HVAC energy to trip energy results
    Updated vehicle model to 'Transit_Bus_Battery_Electric' and integrated HVAC energy calculations into trip energy predictions.

--- scripts/single_agency_full_analysis.py
+++ scripts/single_agency_full_analysis.py
@@ -33,15 +33,16 @@ if __name__ == "__main__":
     import multiprocessing as mp
     import os
     import time
+    import pandas as pd
     from pathlib import Path
 
-    import pandas as pd
-
     from nrel.routee.transit import (
-        aggregate_results_by_trip,
         build_routee_features_with_osm,
         predict_for_all_trips,
+        aggregate_results_by_trip,
     )
+    from nrel.routee.transit.prediction.add_temp_feature import add_HVAC_energy
+
 
     # Suppress GDAL/PROJ warnings, which flood the output when we run gradeit
     # TODO: resolve underlying issue that generates these warnings
@@ -61,7 +62,7 @@ if __name__ == "__main__":
 
     # Set inputs
     n_proc = mp.cpu_count()
-    routee_vehicle_model = "Transit_Bus_Diesel"
+    routee_vehicle_model = "Transit_Bus_Battery_Electric"
     input_directory = HERE / "../sample-inputs/saltlake/gtfs"
     depot_directory = HERE / "../FTA_Depot"
     output_directory = HERE / "../reports/saltlake"
@@ -69,11 +70,13 @@ if __name__ == "__main__":
         output_directory.mkdir(parents=True)
 
     start_time = time.time()
-    routee_input_df, temp_energy_df = build_routee_features_with_osm(
+    routee_input_df, trips_df, feed = build_routee_features_with_osm(
         input_directory=input_directory,
         depot_directory=depot_directory,
         date_incl="2023/08/02",
         routes_incl=["205"],
+        add_between_trip_deadhead=True,
+        add_depot_deadhead=True,
         add_road_grade=True,
         n_processes=n_proc,
     )
@@ -96,8 +99,9 @@ if __name__ == "__main__":
     # Summarize predictions by trip
     energy_by_trip = aggregate_results_by_trip(routee_results, routee_vehicle_model)
 
-    # Add HVAC and BTMS energy to trip
-    energy_by_trip = energy_by_trip.merge(temp_energy_df, on="trip_id", how="left")
+    # Add HVAC energy to trip
+    HVAC_energy_df = add_HVAC_energy(feed, trips_df)
+    energy_by_trip_with_HVAC = energy_by_trip.merge(HVAC_energy_df, on="trip_id", how="left")
 
-    energy_by_trip.to_csv(output_directory / "trip_energy_predictions.csv")
+    energy_by_trip_with_HVAC.to_csv(output_directory / "trip_energy_predictions.csv")
     logger.info(f"Finished predictions in {time.time() - start_time:.2f} s")
